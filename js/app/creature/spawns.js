3c0c5d4e2af8bb735fa8452e5e3e7289cee8cca3
Crypticaz <kyle.pancamo@gmail.com>
2019-03-17 18:29:00 -0500

    Implement 548 data
    - Creature Spawns
    - Gameobject Loot

--- js/app/creature/spawns.js
+++ js/app/creature/spawns.js
@@ -13,70 +13,80 @@
 
     /* The row currently selected by the user (bound to the view) */
     $scope.selected = {
-      guid            : 0,
-      id              : parseInt($stateParams.id, 10),
-      map             : 0,
-      zoneId          : 0,
-      areaId          : 0,
-      spawnMask       : 1,
-      modelid         : 0,
-      equipment_id    : 0,
-      position_x      : 0,
-      position_y      : 0,
-      position_z      : 0,
-      orientation     : 0,
-      spawntimesecs   : 120,
-      spawndist       : 0,
-      currentwaypoint : 0,
-      curhealth       : 1,
-      curmana         : 0,
-      MovementType    : 0,
-      npcflag         : 0,
-      unit_flags      : 0,
-      dynamicflags    : 0,
-      VerifiedBuild   : 0
+      guid            	: 0,
+      id              	: parseInt($stateParams.id, 10),
+      map             	: 0,
+      phaseMask			: 0,
+      spawnDifficulties : 1,
+      PhaseId			: 0,
+      PhaseGroup		: 0,
+      modelid         	: 0,
+      equipment_id    	: 0,
+      position_x      	: 0,
+      position_y      	: 0,
+      position_z      	: 0,
+      orientation     	: 0,
+      spawntimesecs     : 120,
+      spawntimesecsmax  : 120,
+      corpseRemoveTime	: 120,
+      currentwaypoint 	: 0,
+      curhealth       	: 1,
+      curmana         	: 0,
+      MovementType    	: 0,
+      spawndist       	: 0,
+      npcflag           : 0,
+      npcflag2         	: 0,
+      unit_flags		: 0,
+      unit_flags2		: 0,
+      flags_extra		: 0,
+      dynamicflags    	: 0,
+      eventEntry		: 0,
+      pool_entry		: 0,
+      terrainSwapMap	: -1,
+      phaseUseFlags     : 0,
+      zone            	: 0,
+      area            	: 0,
+      areaGroupId		: 0,
+      VerifiedBuild   	: 0
     };
-
-    if ($rootScope.$storage.version.name == "6.x") {
-      $scope.selected.PhaseId    = 0;
-      $scope.selected.PhaseGroup = 0;
-    }
-    else {
-      $scope.selected.phaseMask = 2;
-    }
 
     /* Type check */
     $scope.parseValues = function() {
 
-      $scope.selected.guid            = parseInt($scope.selected.guid, 10);
-      $scope.selected.id              = parseInt($scope.selected.id, 10);
-      $scope.selected.map             = parseInt($scope.selected.map, 10);
-      $scope.selected.zoneId          = parseInt($scope.selected.zoneId, 10);
-      $scope.selected.areaId          = parseInt($scope.selected.areaId, 10);
-      $scope.selected.spawnMask       = parseInt($scope.selected.spawnMask, 10);
-      $scope.selected.modelid         = parseInt($scope.selected.modelid, 10);
-      $scope.selected.equipment_id    = parseInt($scope.selected.equipment_id, 10);
-      $scope.selected.position_x      = parseFloat($scope.selected.position_x, 10);
-      $scope.selected.position_y      = parseFloat($scope.selected.position_y, 10);
-      $scope.selected.position_z      = parseFloat($scope.selected.position_z, 10);
-      $scope.selected.orientation     = parseFloat($scope.selected.orientation, 10);
-      $scope.selected.spawntimesecs   = parseInt($scope.selected.spawntimesecs, 10);
-      $scope.selected.spawndist       = parseFloat($scope.selected.spawndist, 10);
-      $scope.selected.currentwaypoint = parseInt($scope.selected.currentwaypoint, 10);
-      $scope.selected.curhealth       = parseInt($scope.selected.curhealth, 10);
-      $scope.selected.curmana         = parseInt($scope.selected.curmana, 10);
-      $scope.selected.MovementType    = parseInt($scope.selected.MovementType, 10);
-      $scope.selected.npcflag         = parseInt($scope.selected.npcflag, 10);
-      $scope.selected.unit_flags      = parseInt($scope.selected.unit_flags, 10);
-      $scope.selected.dynamicflags    = parseInt($scope.selected.dynamicflags, 10);
-      $scope.selected.VerifiedBuild   = parseInt($scope.selected.VerifiedBuild, 10);
-
-      if ($rootScope.$storage.version.name == "6.x") {
-        $scope.selected.PhaseId       = parseInt($scope.selected.PhaseId, 10);
-        $scope.selected.PhaseGroup    = parseInt($scope.selected.PhaseGroup, 10);
-      } else {
-        $scope.selected.phaseMask      = parseInt($scope.selected.phaseMask, 10);
-      }
+      $scope.selected.guid            		= parseInt($scope.selected.guid, 10);
+      $scope.selected.map             		= parseInt($scope.selected.map, 10);
+      $scope.selected.phaseMask       		= parseInt($scope.selected.phaseMask, 10);
+      $scope.selected.spawnDifficulties		= parseInt($scope.selected.spawnDifficulties, 10);
+      $scope.selected.PhaseId				= parseInt($scope.selected.PhaseId, 10);
+      $scope.selected.PhaseGroup			= parseInt($scope.selected.PhaseGroup, 10);
+      $scope.selected.modelid         		= parseInt($scope.selected.modelid, 10);
+      $scope.selected.equipment_id    		= parseInt($scope.selected.equipment_id, 10);
+      $scope.selected.position_x      		= parseFloat($scope.selected.position_x, 10);
+      $scope.selected.position_y      		= parseFloat($scope.selected.position_y, 10);
+      $scope.selected.position_z      		= parseFloat($scope.selected.position_z, 10);
+      $scope.selected.orientation     		= parseFloat($scope.selected.orientation, 10);
+      $scope.selected.spawntimesecs   		= parseInt($scope.selected.spawntimesecs, 10);
+      $scope.selected.spawntimesecsmax		= parseInt($scope.selected.spawntimesecsmax, 10);
+      $scope.selected.corpseRemoveTime		= parseInt($scope.selected.corpseRemoveTime, 10);
+      $scope.selected.currentwaypoint 		= parseInt($scope.selected.currentwaypoint, 10);
+      $scope.selected.curhealth       		= parseInt($scope.selected.curhealth, 10);
+      $scope.selected.curmana         		= parseInt($scope.selected.curmana, 10);
+      $scope.selected.MovementType    		= parseInt($scope.selected.MovementType, 10);
+      $scope.selected.spawndist       		= parseFloat($scope.selected.spawndist, 10);
+      $scope.selected.npcflag         		= parseInt($scope.selected.npcflag, 10);
+      $scope.selected.npcflag2         		= parseInt($scope.selected.npcflag2, 10);
+      $scope.selected.unit_flags         	= parseInt($scope.selected.unit_flags, 10);
+      $scope.selected.unit_flags2         	= parseInt($scope.selected.unit_flags2, 10);
+      $scope.selected.flags_extra      		= parseInt($scope.selected.flags_extra, 10);
+      $scope.selected.dynamicflags    		= parseInt($scope.selected.dynamicflags, 10);
+      $scope.selected.eventEntry        	= parseInt($scope.selected.eventEntry, 10);
+      $scope.selected.pool_entry         	= parseInt($scope.selected.pool_entry, 10);
+      $scope.selected.terrainSwapMap        = parseInt($scope.selected.terrainSwapMap, 10);
+      $scope.selected.phaseUseFlags         = parseInt($scope.selected.phaseUseFlags, 10);
+      $scope.selected.zone            		= parseInt($scope.selected.zone, 10);
+      $scope.selected.area            		= parseInt($scope.selected.area, 10);
+      $scope.selected.areaGroupId           = parseInt($scope.selected.areaGroupId, 10);
+      $scope.selected.VerifiedBuild   		= parseInt($scope.selected.VerifiedBuild, 10);
 
     };
 
